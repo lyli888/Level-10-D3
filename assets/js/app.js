10b482a3d6882932f37051e010d1150752c58bd3
LL888 <76023384+lyli888@users.noreply.github.com>
2021-04-28 16:30:13 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -57,14 +57,33 @@ d3.csv("/data/data.csv").then(function(stateData) {
     .call(leftAxis);
 
     
+  // Create Circles
+  var circleLabels = chartGroup.selectAll(null).data(stateData).enter().append("text")
+    .data(stateData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", 15)
+    .attr("class", "blue")
+    .attr("opacity", ".5");
+
+  circleLabels
+    .attr("x", function(d) { return d.poverty; })
+    .attr("y", function(d) { return d.healthcare; })
+    .text(function(d) { return d.abbr; })
+    .attr("font-family", "sans-serif")
+    .attr("font-size", "5px")
+    .attr("fill", "white");
+
 //Axes labels
-chartGroup.append("text")
-  .attr("transform", "rotate(-90)")
-  .attr("y", 0 - margin.left + 40)
-  .attr("x", 0 - (height / 2))
-  .attr("dy", "1em")
-  .attr("class", "axisText")
-  .text("% Without Healthcare");
+  chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left + 40)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .attr("class", "axisText")
+    .text("% Without Healthcare");
 
 chartGroup.append("text")
     .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
