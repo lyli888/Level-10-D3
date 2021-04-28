752920bf20eca93b7b7dd04abc585859cffb1ee9
LL888 <76023384+lyli888@users.noreply.github.com>
2021-04-28 15:19:21 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,10 +29,9 @@ d3.csv("data.csv").then(function(stateData, err) {
   if (err) throw err;
 
   // parse data
-  thisData.forEach(function(data) {
+  stateData.forEach(function(data) {
     data.poverty = +data.poverty;
     data.obesity = +data.healthcare;
-
   });
 
   // Create x scale function
@@ -63,87 +62,40 @@ d3.csv("data.csv").then(function(stateData, err) {
     .data(stateData)
     .enter()
     .append("circle")
-    .attr("cx", d => xLinearScale(d[chosenXAxis]))
-    .attr("cy", d => yLinearScale(d.num_hits))
-    .attr("r", 20)
-    .attr("fill", "pink")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", 15)
+    .attr("class", "stateCircle")
     .attr("opacity", ".5");
 
-  // Create group for two x-axis labels
-  var labelsGroup = chartGroup.append("g")
-    .attr("transform", `translate(${width / 2}, ${height + 20})`);
 
-  var hairLengthLabel = labelsGroup.append("text")
-    .attr("x", 0)
-    .attr("y", 20)
-    .attr("value", "hair_length") // value to grab for event listener
-    .classed("active", true)
-    .text("Hair Metal Ban Hair Length (inches)");
+    chartGroup.selectAll("div")
+    .data(povertyData)
+    .enter()
+    .append("text")
+    .text(function(d){
+      console.log(d.abbr)
+      return `${d.abbr}`;
+    })
+  .attr("x", function (d) {
+      return xLinearScale(d.poverty);
+    })
+  .attr("y", function (d) {
+      return yLinearScale(d.healthcare);
+    })
+  .attr("alignment-baseline", "central")
+  .attr("class", "stateText");
 
-  var albumsLabel = labelsGroup.append("text")
-    .attr("x", 0)
-    .attr("y", 40)
-    .attr("value", "num_albums") // value to grab for event listener
-    .classed("inactive", true)
-    .text("# of Albums Released");
+  // Tooltip Function
+  
+  var tooltipfunction = d3.tip()
 
-  // append y axis
-  chartGroup.append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 0 - margin.left)
-    .attr("x", 0 - (height / 2))
-    .attr("dy", "1em")
-    .classed("axis-text", true)
-    .text("Number of Billboard 500 Hits");
+  
+        
 
-  // updateToolTip function above csv import
-  var circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
 
-  // x axis labels event listener
-  labelsGroup.selectAll("text")
-    .on("click", function() {
-      // get value of selection
-      var value = d3.select(this).attr("value");
-      if (value !== chosenXAxis) {
-
-        // replaces chosenXAxis with value
-        chosenXAxis = value;
-
-        // console.log(chosenXAxis)
-
-        // functions here found above csv import
-        // updates x scale for new data
-        xLinearScale = xScale(hairData, chosenXAxis);
-
-        // updates x axis with transition
-        xAxis = renderAxes(xLinearScale, xAxis);
-
-        // updates circles with new x values
-        circlesGroup = renderCircles(circlesGroup, xLinearScale, chosenXAxis);
-
-        // updates tooltips with new info
-        circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
-
-        // changes classes to change bold text
-        if (chosenXAxis === "num_albums") {
-          albumsLabel
-            .classed("active", true)
-            .classed("inactive", false);
-          hairLengthLabel
-            .classed("active", false)
-            .classed("inactive", true);
-        }
-        else {
-          albumsLabel
-            .classed("active", false)
-            .classed("inactive", true);
-          hairLengthLabel
-            .classed("active", true)
-            .classed("inactive", false);
-        }
-      }
-    });
 }).catch(function(error) {
   console.log(error);
+
 });
 
