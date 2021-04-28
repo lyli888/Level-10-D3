83a9d069d8a60d4ea4429f1d8f89d8fed9a57e1e
LL888 <76023384+lyli888@users.noreply.github.com>
2021-04-28 15:25:15 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -57,7 +57,7 @@ d3.csv("data.csv").then(function(stateData, err) {
   chartGroup.append("g")
     .call(leftAxis);
 
-  // append initial circles
+  // Create Circles
   var circlesGroup = chartGroup.selectAll("circle")
     .data(stateData)
     .enter()
@@ -86,13 +86,31 @@ d3.csv("data.csv").then(function(stateData, err) {
   .attr("alignment-baseline", "central")
   .attr("class", "stateText");
 
-  // Tooltip Function
-  
-  var tooltipfunction = d3.tip()
+// Tooltip Function
 
-  
-        
+//Event Listeners
 
+circlesGroup.on("click", function(data) {
+    toolTip.show(data, this);
+    })
+    // onmouseout event
+    .on("mouseout", function(data, index) {
+        toolTip.hide(data);
+    });
+
+//Axes labels
+chartGroup.append("text")
+.attr("transform", "rotate(-90)")
+.attr("y", 0 - margin.left + 40)
+.attr("x", 0 - (height / 2))
+.attr("dy", "1em")
+.attr("class", "axisText")
+.text("Lacks Healthcare (%)");
+
+chartGroup.append("text")
+      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("class", "axisText")
+      .text("% In Poverty");
 
 }).catch(function(error) {
   console.log(error);
