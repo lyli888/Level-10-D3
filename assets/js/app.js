5d4e9a85c363d6ffeef48f7e8e5a37239ddf05e5
LL888 <76023384+lyli888@users.noreply.github.com>
2021-04-28 16:27:45 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -59,17 +59,17 @@ d3.csv("/data/data.csv").then(function(stateData) {
     
 //Axes labels
 chartGroup.append("text")
-.attr("transform", "rotate(-90)")
-.attr("y", 0 - margin.left + 40)
-.attr("x", 0 - (height / 2))
-.attr("dy", "1em")
-.attr("class", "axisText")
-.text("% Without Healthcare");
+  .attr("transform", "rotate(-90)")
+  .attr("y", 0 - margin.left + 40)
+  .attr("x", 0 - (height / 2))
+  .attr("dy", "1em")
+  .attr("class", "axisText")
+  .text("% Without Healthcare");
 
 chartGroup.append("text")
     .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
-      .attr("class", "axisText")
-      .text("% In Poverty");
+    .attr("class", "axisText")
+    .text("% In Poverty");
 
 }).catch(function(error) {
   console.log(error);
